9c1459c85e542100a7bf95929478ed16b06b29e8
fitnr <fitnr@fakeisthenewreal>
2012-07-22 14:36:30 -0400

    i dont' think dbf module is going to do it. bug city

--- census2dbf.py
+++ census2dbf.py
@@ -5,14 +5,69 @@ csv2dbf.py
 Created by Neil Freeman on 2012-06-10.
 """
 
+import os
 import sys
 import argparse
 import dbf
 import csv
 import re
-from datetime import datetime
 
 help_message = '''Convert CSV to DBF. Requires python dbf library: http://pypi.python.org/pypi/dbf/'''
+
+
+def local_from_csv(csvfile='', reader=False, to_disk=False, filename=None,
+    field_names=None, extra_fields=None, dbf_type='db3', memo_size=64, min_field_size=1):
+
+    'Adapted from the dbf module to allow for using an open csv reader'
+    """creates a Character table from a csv file to_disk will create a table with the same name
+    filename will be used if provided field_names default to f0, f1, f2, etc, unless specified (list)
+    extra_fields can be used to add additional fields -- should be normal field specifiers (list)"""
+
+    if not reader:
+        reader = csv.reader(open(csvfile))
+    if field_names:
+        if ' ' not in field_names[0]:
+            field_names = ['%s M' % fn for fn in field_names]
+    else:
+        field_names = ['f0 M']
+    print 'field_names:\n' + str(field_names)
+    mtable = dbf.Table(':memory:', [field_names[0]], dbf_type=dbf_type, memo_size=memo_size)
+    fields_so_far = 1
+    for row in reader:
+        while fields_so_far < len(row):
+            if fields_so_far == len(field_names):
+                field_names.append('f%d M' % fields_so_far)
+            mtable.add_fields(field_names[fields_so_far])
+            fields_so_far += 1
+        mtable.append(tuple(row))
+    if filename:
+        to_disk = True
+
+    if not to_disk:
+        if extra_fields:
+            mtable.add_fields(extra_fields)
+    else:  # If writing to disk.
+        if not filename:
+            filename = os.path.splitext(csvfile)[0]
+        length = [min_field_size] * len(field_names)
+        for record in mtable:
+            for i in dbf.index(record.field_names):
+                length[i] = max(length[i], len(record[i]))
+        fields = mtable.field_names
+        print 'mtable.field_names:\n' + str(fields)
+        fielddef = []
+        for i in dbf.index(length):
+            if length[i] < 255:
+                fielddef.append('%s C(%d)' % (fields[i], length[i]))
+            else:
+                fielddef.append('%s M' % (fields[i]))
+        if extra_fields:
+            fielddef.extend(extra_fields)
+        csvtable = dbf.Table(filename, fielddef, dbf_type=dbf_type)
+        for record in mtable:
+            csvtable.append(record.scatter_fields())
+        return csvtable
+    return mtable
 
 
 class Usage(Exception):
@@ -20,161 +75,133 @@ class Usage(Exception):
         self.msg = msg
 
 
-class parse_csv():
+class parse_csv(object):
     # open up the csv, decide what kind of format it is, and get the fields and data ready.
     # input: file handle
     # input (optional): field types, date format
     # output: fieldnames, lengths, types
 
     def __init__(self, **kwargs):
-        self.input_handle = kwargs.get('input_handle')
+        self.handle = kwargs.get('handle')
         self.fields = kwargs.get('fields', None)
-        self.date_format = kwargs.get('date_format', None)
+        self.reader = csv.reader(self.handle)
 
-    def parse_header(self, header):
+    def parse_header(self, num_header_rows):
         'parses a row and returns fieldnames'
-        fields = header.strip().split(',')
-        fields = [f[:11] for f in fields]  # Clip each field to 11 characters.
-        # Replace illegal characters in fields with underscore.
-        illegal = re.compile(r'[^\w]')
-        return [illegal.sub('_', x) for x in fields]
+        if num_header_rows == 0:
+            return False
+        self.handle.seek(0)
+        full_header = []
+        for k in range(num_header_rows):
+            row = next(self.reader)
+            if (0 == k):
+                header = row
+            full_header.append(row)
 
-    def convert_type(self, value):
-        'Value should be a string read from the csv. Will return value in the (possibly) correct type.'
-        if self.datetime_format:
+        header = [f[:11] for f in header]  # Clip each field to 11 characters.
+        if header[0] == '':
+            header[0] = 'geoid'
+        if header[1] == '':
+            header[1] = 'geoid2'
+        if header[2] == '':
+            header[2] = 'geoname'
+        # Replace illegal characters in header with underscore.
+        illegal = re.compile(r'[^\w]')
+        full_header = zip(*full_header)
+        header = [illegal.sub('_', x) for x in header]
+        return full_header, header
+
+    def find_first_data_row(self):
+        self.handle.seek(0)
+        for i in range(10):
+            row = self.reader.next()
+            if re.match(r'\d{7}US\d{2}', row[0]):
+                self.handle.seek(0)
+                # We found some census data, return the row number
+                return i
+        return False
+
+    def find_first_num_field(self, num_header_rows):
+        self.handle.seek(0)
+        for i in range(num_header_rows):
+            self.reader.next()
+        row = self.reader.next()
+        # Census files always start with two GEOIDs. Right?
+        for j in range(2, len(row)):
+            field = row[j]
             try:
-                return datetime.strptime(value, self.datetime_format)
+                int(field)
+                return j
             except ValueError:
                 pass
+        return False
 
-        try:
-            return datetime.strptime(value, "%Y/%m/%d")
-        except ValueError:
-            pass
-
-        try:
-            return datetime.strptime(value, "%m/%d/%Y")
-        except ValueError:
-            pass
-
-        try:
-            if float(value) == int(value):
-                return int(value)
+    def spec_fields(self, header, first_num_field):
+        if self.fields:
+            return self.fields
+        fieldspecs = []
+        # fieldspecs are going to look like: ["stringfield C", "numfield N"]
+        for field in header:
+            #fieldspecs.append(field)
+            if (first_num_field > 0):
+                fieldspecs.append(field + ' C')
+                first_num_field -= 1
             else:
-                return float(value)
-        except ValueError:
-            pass
+                fieldspecs.append(field + ' N')
 
-        # Return the string.
-        return value
+        return fieldspecs
 
-    def parse_field_lengths(self, handle):
-        'parses a handle and returns maxlengths of fields in each column'
-        reader = csv.reader(handle)
-        lengths = next(reader)
-        lengths = [len(x) for x in lengths]
-        for row in reader:
-            e = enumerate(row)
-            for x, y in e:
-                lengths[x] = max(lengths[x], len(y))
-        handle.seek(0)
-        return lengths
-
-    def detect_census(self, handle):
-        reader = csv.reader(handle)
-        for i in range(10):
-            row = next(reader)
-            if re.match(r'\d{7}US\d{2}', row[0]) is not None:
-                handle.seek(0)  # Reset handle to start.
-                return True, i  # We found some census data, return the row number
-
-        handle.seek(0)  # Reset handle to start.
-        return False, 0
-
-    def read_handle(self):
-        census, first_row = self.detect_census()
-        if census:
-            # Read as a census file.
-            # Namely, skip extra header rows and make a data dictionary.
-            pass
-        pass
-
-    def read_quote_minimal(self):
-        'Quote_minimal reads everything as a string'
-        header = self.input_handle.next()
-        headers = parse_header(header)
-        quote_minimal_reader = csv.reader(self.input_handle, quoting=csv.QUOTE_MINIMAL)
-        quote_minimal_rows = quote_minimal_reader.next()
-        # Reset to the first row to read it with csv module.
-        input_handle.seek(0)
-        input_handle.next()
-
-    def read_quote_nonnumeric(self):
-        try:
-            quote_nonnumeric_reader = csv.reader(input_handle, quoting=csv.QUOTE_NONNUMERIC)
-            # stringreader will that a bunch of ints are float, but is correct about strings.
-            quote_nonnumeric_rows = quote_nonnumeric_reader.next()
-        except ValueError:
-            sys.exit("Could not process document.\nPlease make sure that fields are properly escaped.")
-
-    def combine_rows(self, non_numeric_rows, minimal_rows):
-        two_rows = zip(non_numeric_rows, minimal_rows)
-        field_types = []
-
-        decimal = re.compile(r'\.')
-
-        for x in two_rows:
-            if isinstance(x[0], str) and isinstance(x[1], str):
-                field_types.append('C')
-
-            elif decimal.search(x[1]):
-                field_types.append('F')
-
-            else:
-                field_types.append('N')
-
-        return field_types
-    #print dbf_fields
-
-    reader = csv.reader(self.input_handle, quoting=csv.QUOTE_MINIMAL)
-    records = []
-    for row in reader:
-        records.append(row)
-
-    field_defs = zip(field_types, field_lengths)
-    precision = lambda x: 2 if (x == 'F') else 0
-    field_defs = [(x, y, precision(x)) for x, y in field_defs]
-
-
-class output_dbf():
-    # uses with dbf class
-    # input: fieldnames, lengths, types
-    # input: destination file name (optional)
-    # output: dbf file
-    def __init__(self, **kwargs):
-        pass
+    def point_at_data(self, num_header_rows):
+        self.handle.seek(0)
+        for i in range(num_header_rows):
+            self.reader.next()
 
 
 def main(argv=None):
     description = 'Convert CSV to DBF.'
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('input', type=argparse.FileType('rb', 0), help='input csv')
-    parser.add_argument('output', type=argparse.FileType('wb', 0), help='output dbf')
+    parser.add_argument('output', help='output dbf file name')
+    parser.add_argument('output_dd', default=False, help='output a data dictionary made from the header')
 
     args = parser.parse_args()
-    input_handle = args.input
 
     csv_parser_args = {
-        'input_handle': input_handle
+        'handle': args.input
     }
-    csv_parser = parse_csv(csv_parser_args)
+    csv_parser = parse_csv(**csv_parser_args)
+    num_header_rows = csv_parser.find_first_data_row()
 
-    #csv_parser.dostuff()
+    data_dictionary, header = csv_parser.parse_header(num_header_rows)
 
-    # Calculate field lengths
-    field_lengths = csv_parser.parse_field_lengths()
+    # Write the field specs.
+    first_num_field = csv_parser.find_first_num_field(num_header_rows)
 
-    #db = dbf.dbfwriter(args.output, headers, field_defs, records)
+    fieldspecs = csv_parser.spec_fields(header, first_num_field)
+    print 'fieldspecs :\n' + str(fieldspecs)
+    #place pointer in the right place
+    csv_parser.point_at_data(num_header_rows)
+
+    #local_from_csv(reader=csv_parser.reader, field_names=fieldspecs, filename=args.output, dbf_type='dbf')
+    #mtable = local_from_csv(reader=csv_parser.reader, field_names=fieldspecs, dbf_type='dbf')
+    #mtable = local_from_csv(reader=csv_parser.reader, dbf_type='dbf')
+    fields = ['GEOid', 'GEOid2','GEOdisplaylabel','S01','S01','S02','S02','S03','S03','S04','S04','S05','S05','S06','S06','S07','S07','S08','S08','S09','S09','S10','S10','S11','S11','S12','S12','S13','S13','S14','S14','S15','S15','S16','S16','S17','S17','S18','S18','S19','S19','S20','S20','S21','S21','S22','S22','S23','S23','S24','S24','S25','S25','S26','S26','S27','S27','S28','S28','S29','S29','S30','S30','S31','S31','S32','S32','S33','S33','S34','S34','S35','S35','S36','S36','S37','S37','S38','S38','S39','S39','S40','S40','S41','S41','S42','S42','S43','S43','S44','S44','S45','S45','S46','S46','S47','S47','S48','S48','S49','S49','S50','S50','S51','S51'
+    ]
+
+    mtable = dbf.from_csv(csvfile='test2.csv', filename=args.output, field_names=fields)
+    print mtable
+
+    if args.output_dd:
+        output = args.output[0:-4] + "_data-dictionary.txt"
+        with open(output, 'w+') as data_dict_handle:
+            data_dict_handle.write("Data dictionary extracted from the header of " % args.input.name)
+            out_list = []
+            for row in data_dictionary:
+                out_str = ""
+                for field in row:
+                    out_str = out_str + field + "\t"
+                out_list.appen(out_str)
+            data_dictionary.writelines(out_str)
 
 
 if __name__ == "__main__":
